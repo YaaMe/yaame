024a0ac289e784df35b316558210cd752b57180b
YaaMe <yaame@yaameMacBook.local>
2018-03-18 20:10:33 +0900

    feat(humanLanguage): check answer

--- humanLanguage/japanese.js
+++ humanLanguage/japanese.js
@@ -8,4 +8,71 @@ let $random = range => {
 
 const UPPER = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
 const LOWER = "アイウエオカキクエコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン";
-console.log($random(`${UPPER}${LOWER}`))
+const ANSWER = [
+    'a',
+    'i',
+    'u',
+    'e',
+    'o',
+    'ka',
+    'ki',
+    'ku',
+    'ke',
+    'ko',
+    'sa',
+    'shi',
+    'su',
+    'se',
+    'so',
+    'ta',
+    'chi',
+    'tsu',
+    'te',
+    'to',
+    'na',
+    'ni',
+    'nu',
+    'ne',
+    'no',
+    'ha',
+    'hi',
+    'fu',
+    'he',
+    'ho',
+    'ma',
+    'mi',
+    'mu',
+    'me',
+    'mo',
+    'ya',
+    'yu',
+    'yo',
+    'ra',
+    'ri',
+    'ru',
+    're',
+    'ro',
+    'wa',
+    'wo',
+    'n'
+]
+let readline = require('readline');
+
+let bot = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+let range = `${UPPER}${LOWER}`;
+let ask = question => {
+    bot.question(`${question}\n`, read => {
+        let answer = ANSWER[range.indexOf(question) % ANSWER.length];
+        if (answer == read) {
+            return ask($random(range))
+        } else {
+            console.log(`answer is ${answer}`)
+            bot.close();
+        }
+    });
+}
+ask($random(range))
+
